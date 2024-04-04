10896d4ad760604b2a6592842b7d266992f88210
Oliver0731 <o.townell@share.epsb.ca>
2024-04-04 09:59:56 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,71 @@
-function myIncludes(array, item) {
-  let indicator = 0;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] == item) {
-      i = array.length;
-      return true;
-      indicator = 1;
-    }
-  }
-  if ((indicator === 0) | (indicator < 0)) {
-    return false;
-  }
-}
-let names = ["Amy", "Ben", "Carl"];
-if (myIncludes(names, "Ben")) {
-  console.log(`Hello Ben`);
-} else {
-  console.log("where is ben");
-}
-if (myIncludes(names, "Oliver")) {
-  console.log(`Hello Oliver`);
-} else {
-  console.log("where is Oliver");
-}
-if (myIncludes(names, "Amy")) {
-  console.log(`Hello Amy`);
-} else {
-  console.log("where is Amy");
-}
-if (myIncludes(names, "Carl")) {
-  console.log(`Hello Carl`);
-} else {
-  console.log("where is Carl");
-}
+let names = ["Amy", "Ben", "Carl", "Amy", "Ben"];
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 1000];
+///////////////////////////////////////////////////////////////////////////
 
-function myIndexOf(array, item) {}
-function average() {}
-function count() {}
+//INCLUDES
+// function myIncludes(array, item) {
+//   let indicator = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] == item) {
+//       i = array.length;
+//       return true;
+//       indicator = 1;
+//     }
+//   }
+//   if ((indicator === 0) | (indicator < 0)) {
+//     return false;
+//   }
+// }
+// if (myIncludes(names, "Ben")) {
+//   console.log(`Hello Ben`);
+// } else {
+//   console.log("where is ben");
+// }
+// if (myIncludes(names, "Oliver")) {
+//   console.log(`Hello Oliver`);
+// } else {
+//   console.log("where is Oliver");
+// }
+// if (myIncludes(names, "Amy")) {
+//   console.log(`Hello Amy`);
+// } else {
+//   console.log("where is Amy");
+// }
+// if (myIncludes(names, "Carl")) {
+//   console.log(`Hello Carl`);
+// } else {
+//   console.log("where is Carl");
+// }
+
+////////////////////////////////////////////////////////////////////////
+
+// INDEXOF
+
+// function myIndexOf(array, item) {
+//   let determin = 0;
+
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] == item) {
+//       determin = i;
+//       i = array.length;
+//     } else {
+//       determin = -1;
+//     }
+//   }
+//   return determin;
+// }
+// console.log(myIndexOf(names, "Ben"));
+// console.log(myIndexOf(names, "Oliver"));
+
+// function average(array) {
+//   let total = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     total += array[i];
+//   }
+//   return total / array.length;
+// }
+// console.log(average(nums));
+
+function count(array, item) {}
 function myMax() {}
 function swap() {}
